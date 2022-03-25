6a54336cd86e3ccbf39b7f2511fbc67a3e1ce80e
jason a. grafft <jgrafft@gmail.com>
2022-03-25 16:08:57 -0500

    Renamed function parameters for clarity

--- rel_query_transform.py
+++ rel_query_transform.py
@@ -2,31 +2,28 @@ from functools import reduce
 from pandas import DataFrame
 
 
-"""
-Invoke `single_key_relation_to_DF` or `multi_key_relation_to_DF` based on length of
-`output` key in dictionary returned from RAI.
-"""
-def to_DF(rel, cols=None, df=True):
+def to_DF(rel, columns=None, return_df=True):
+    """
+    Invoke `single_key_relation_to_DF` or `multi_key_relation_to_DF` based on length of
+    `output` key in dictionary returned from RAI.
+    """
     return (
-        # len(rel["output"]) == 1
-        single_key_relation_to_DF(rel, df, cols=cols)
+        single_key_relation_to_DF(rel, return_df, columns=columns)
         if len(rel["output"]) == 1
-        # len(rel["output"]) > 1
-        else multikey_relation_to_DF(rel, df)
+        else multikey_relation_to_DF(rel, return_df)
     )
 
 
-"""
-Transform a multikey relation with a "ragged right" structure (e.g. not all relations
-are the same cardinality) to a Pandas `DataFrame` (default) or Python `dict`.
+def multikey_relation_to_DF(rel, return_df):
+    """
+    Transform a multikey relation with a 'ragged right' structure (e.g. not all relations
+    are the same cardinality) to a Pandas `DataFrame` (default) or Python `dict`.
 
-NB. Function fills "missing" keys in each relation with the value `None`.
-"""
-def multikey_relation_to_DF(rel, df):
+    NB. Function fills 'missing' keys in each relation with the value `None`.
     """
-    Traverse all `dict`s in the `output` array and add the contents of
-    the first index of `columns`--the keys for our data--to a `set`.
-    """
+
+    # Traverse all `dict`s in the `output` array and add the contents of
+    # the first index of `columns`--the keys for our data--to a `set`.
     global_key_set = set(
         reduce(lambda X, Y: X + Y, [x["columns"][0] for x in rel["output"]])
     )
@@ -34,10 +31,8 @@ def multikey_relation_to_DF(rel, df):
     # Used in the `for` loop below
     data = {}
 
-    """
-    Traverse all `dict`s in the `output` array (again) and insert `None` values
-    for missing keys in each relation.
-    """
+    # Traverse all `dict`s in the `output` array (again) and insert `None` values
+    # for missing keys in each relation.
     for X in rel["output"]:
         # Create a `dict` of key-value pairs for the data included in the current relation.
         key_value_pairs = {
@@ -45,7 +40,7 @@ def multikey_relation_to_DF(rel, df):
         }
 
         """
-        Create key in `data` to store "polyfilled" relation (e.g. relation with `None`s) then
+        Create key in `data` to store 'polyfilled' relation (e.g. relation with `None`s) then
         iterate `global_key_set` and check whether each of its members is included in `key_value_pairs`.
 
         Rules
@@ -57,13 +52,13 @@ def multikey_relation_to_DF(rel, df):
         ]
 
     # Return a Pandas `DataFrame` or Python `dict`
-    return (DataFrame(data) if df else data)
+    return DataFrame(data) if return_df else data
 
 
-"""
-Transform a single key relation to a Pandas `DataFrame` or Python `dict`.
-"""
-def single_key_relation_to_DF(rel, df, columns=None):
+def single_key_relation_to_DF(rel, return_df, columns=None):
+    """
+    Transform a single key relation to a Pandas `DataFrame` or Python `dict`.
+    """
     if columns is not None:
         # If user provided column names, ensure there are exactly enough for available data relations.
         if len(columns) != len(rel["output"][0]["columns"]):
@@ -73,9 +68,9 @@ def single_key_relation_to_DF(rel, df, columns=None):
             d = {columns[i]: v for (i, v) in enumerate(rel["output"][0]["columns"])}
 
             # Return `DataFrame` or `dict` of key-value pairs where keys populate from `columns`.
-            return (DataFrame(d) if df else d)
+            return DataFrame(d) if return_df else d
     else:
         d = {f"x{i}": v for (i, v) in enumerate(rel["output"][0]["columns"])}
 
         # Return `DataFrame` or `dict` of key-value pairs where keys are generated automatically.
-        return (DataFrame(d) if df else d)
+        return DataFrame(d) if return_df else d
